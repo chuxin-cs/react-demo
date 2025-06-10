53e12cdad1580609e3787c147f6968b28c55280f
chuxin <15115359055@139.com>
2025-06-10 15:26:45 +0800

    feat(router): 添加基础路由组件和系统页面
    - 创建路由组件框架
    - 添加系统错误页面(403/404/500/通用错误)
    - 添加登录页面
    - 在App中预留配置式路由位置

--- demo-react-router/src/App.tsx
+++ demo-react-router/src/App.tsx
@@ -4,8 +4,12 @@ export default function App() {
   return (
     <>
       <h1>React Router</h1>
+
       {/* DOM 方式 */}
       <RouterDomDemo />
+
+      {/* 配置式 */}
+      
     </>
   );
 }
---
+++ demo-react-router/src/router/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Router: React.RC = () => {};
+
+export default Router;
