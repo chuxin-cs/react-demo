f0adeac30a4c2acb89abe5e7cd824b9c1ef51b7a
chuxin <15115359055@139.com>
2025-06-10 10:28:10 +0800

    feat: 添加全局flex样式并更新App组件
    - 新增global.module.css文件定义flex样式
    - 在App组件中使用新增的flex样式
    - 移除package.json中的private字段

--- demo-style/src/App.tsx
+++ demo-style/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import globalStyle from './global.module.css';
 
 function App() {
   return (
     <div>
-      <div className={'css_style'}></div>
+      <div className={'css_style'}>1</div>
+      <div className={globalStyle.flex}>2</div>
     </div>
   );
 }
