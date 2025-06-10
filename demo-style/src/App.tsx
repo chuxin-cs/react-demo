0b2425d6a3eb2887f6424c729be389e1b907b3bc
chuxin <15115359055@139.com>
2025-06-10 10:41:55 +0800

    refactor(styled-components): 修复样式组件属性传递问题
    - 将 StyleButton 的 bg 属性改为 $bg 以避免 React 警告
    - 新增 StyleButtonWithConfig 组件，使用 shouldForwardProp 过滤 bg 属性

--- demo-style/src/App.tsx
+++ demo-style/src/App.tsx
@@ -3,14 +3,26 @@ import globalStyle from './global.module.css';
 
 import styled from 'styled-components';
 interface StyleButtonProps {
-  bg?: boolean;
+  $bg?: boolean;
 }
 //                  styled.* 范围（html元素，或者组件）
 const StyleButton = styled.button<StyleButtonProps>`
   color: #fff;
   border: 0;
-  background: ${(props) => (props.bg ? '#000' : '#666')};
+  background: ${(props) => (props.$bg ? '#000' : '#666')};
 `;
+
+// shouldForwardProp
+interface StyleButtonProps1 {
+  bg?: boolean;
+}
+const StyleButtonWithConfig = styled.button.withConfig({
+  shouldForwardProp: (prop) => !['bg'].includes(prop) // 过滤 bg 属性
+})<StyleButtonProps1>`
+color: #fff;
+border: 0;
+background: ${(props) => (props.bg ? "#000" : "#666")};
+`
 
 function App() {
   return (
@@ -18,7 +30,8 @@ function App() {
       <div className={'css_style'}>1</div>
       <div className={globalStyle.flex}>2</div>
       {/* 这里直接绑定 bg 会被渲染到 button dom 上 但是button没有这个属性 react会抛出警告 Warning: Received `true` for a non-boolean attribute `bg` */}
-      <StyleButton bg={true}>3</StyleButton>
+      <StyleButton $bg={true}>3</StyleButton>
+      <StyleButtonWithConfig bg={true}>4</StyleButtonWithConfig>
     </div>
   );
 }
