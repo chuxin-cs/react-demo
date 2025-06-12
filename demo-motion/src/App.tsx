2f6f7b81edc7c363c604e8c72ca43ffcf7398f79
chuxin <15115359055@139.com>
2025-06-12 17:51:51 +0800

    feat: 添加旋转动画功能及motion依赖
    - 新增app.css定义旋转元素的样式
    - 添加motion依赖包
    - 在App.tsx中实现360度旋转动画组件

--- demo-motion/src/App.tsx
+++ demo-motion/src/App.tsx
@@ -1,7 +1,26 @@
+import './app.css';
 import { useState } from 'react';
+import { motion } from 'motion/react';
+
+function Rotate() {
+  return (
+    <motion.div
+      className={'box'}
+      animate={{ rotate: 360 }}
+      transition={{ duration: 5 }}
+    />
+  );
+}
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <h1>react motion</h1>
+
+      {/* 360度旋转 */}
+      <Rotate />
+    </>
+  );
 }
 
 export default App;
